3a2f057fff08ffa79fa443298e5bc7dacf673bce
Hannah Neal <hannahmneal@gmail.com>
2019-01-09 11:45:53 -0600

    initialized project files and packages

---
+++ src/lib/grunt/browserify.js
@@ -0,0 +1,28 @@
+module.exports = {
+  options: {
+    transform: [
+      [
+        "babelify",
+        {
+          "presets": [
+            [
+              "@babel/preset-env", {
+                "targets": {
+                  "node":
+                  "current"
+                }
+              }
+            ]
+          ]
+        }
+      ]
+    ],
+    browserifyOptions: {
+      debug: true,
+    }
+  },
+  app: {
+    src: ["../scripts/main.js"],
+    dest: "../../public/bundle.js"
+  }
+  }
