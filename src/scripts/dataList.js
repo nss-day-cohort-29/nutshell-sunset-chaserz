336b0cfadaa99a1ef369831514898d6887a527cf
Zac Crawford <zwcrawford@gmail.com>
2019-01-11 10:47:53 -0600

    switched from innerHTML to createElement for the registration form, doing handleNewUser method next

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -1,1 +1,5 @@
 // Here is where the second promises can be listed, imported from dataCollection.js
+
+// import dataCollection from "./dataCollection"
+// import cards from "./cards"
+
--- src/scripts/registration.js
+++ src/scripts/registration.js
@@ -1,28 +1,54 @@
-
-//import dataCollection from "./dataCollection"
+// Zac
+import dataCollection from "./dataCollection"
 
 const registrationForm = {
   createAndAppendForm () {
-    let registrationDiv = document.getElementById("regForm")
-    registrationDiv.innerHTML = `
-      <form>
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" autocomplete="off">
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" autocomplete="off">
-      </div>
-      <div class="form-group form-check">
-      </div>
-      <button name="newAcctButton" type="submit" class="btn btn-primary">New Account</button>
-      <button name="loginButton" type="submit" class="btn btn-primary">Login</button>
-      </form>`
+
+    // 1. Build HTML form...like a boss!
+    let formHeader = document.createElement("h3")
+    formHeader.textContent = "Welcome to Nutshell!"
+    let emailField = document.createElement("fieldset")
+    let emailLabel = document.createElement("label")
+    emailLabel.textContent = "Email"
+    emailLabel.setAttribute("for", "email")
+    let emailInput = document.createElement("input")
+    emailInput.setAttribute("id", "email")
+    emailInput.setAttribute("name", "email")
+
+    emailField.appendChild(emailLabel)
+    emailField.appendChild(emailInput)
+
+    let passField = document.createElement("fieldset")
+    let passLabel = document.createElement("label")
+    passLabel.textContent = "Password"
+    passLabel.setAttribute("for", "password")
+    let passInput = document.createElement("input")
+    passInput.setAttribute("id", "password")
+    passInput.setAttribute("name", "password")
+
+    passField.appendChild(passwordLabel)
+    passField.appendChild(passwordInput)
+
+    let newAcctBtn = document.createElement("button")
+    newAcctBtn.textContent = "New Account"
+    newAcctBtn.setAttribute("class", "new__acct")
+
+    newAcctBtn.addEventListener("click", this.handleAddNewUser)
+
+    //Refer to div on index with id "regForm"
+    // 2. Create an empty frag and build it up
+    let registrationFormFrag = document.createDocumentFragment()
+    registrationFormFrag.appendChild(formHeader)
+    registrationFormFrag.appendChild(emailField)
+    registrationFormFrag.appendChild(passField)
+    registrationFormFrag.appendChild(newAcctBtn)
+
+    // 3. Target the div
+    let registrationArticle = document.querySelector("#regForm")
+
+    // 4. Append frag to div
+    registrationArticle.appendChild(registrationFormFrag)
   },
-  handleAddNewUser (event) {
-    let inputUserEmail = document.querySelector("#exampleInputEmail1").value
-    let inputUserPassword = document.querySelector("#exampleInputPassword1").value
-  }
+  handleAddNewUser
 }
 export default registrationForm
